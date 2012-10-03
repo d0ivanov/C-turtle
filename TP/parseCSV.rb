10333d691bd5b325dcf1a0d97d09ba28cf38198f
starbust <syliconsynapse@gmail.com>
2012-10-03 16:55:32 +0300

    Aded function which uses the CSV class. Added functionality to calculate total problems solved in ruby, shell, python and student number sum.

--- TP/parseCSV.rb
+++ TP/parseCSV.rb
@@ -1,15 +1,34 @@
 #!/usr/bin/env ruby
 #execute with either ruby filename.rb
 #or chmod +x filename.rb and ./filename.rb
-result =  0
-bool = true
-File.new("test.csv", 'r').each_line("\n") do |row|
-	if(bool)#so that we do not parse first (header) row
-		bool = false
-	else
+require "csv"
+
+def parse_csv
+	py = 0
+	rb = 0
+	sh = 0
+	sum = 0
+	CSV.foreach("test.csv") do |row|
+		case row[5].downcase
+			when "ruby" then rb += 1
+			when "python" then py += 1
+			when "shell" then sh += 1
+		end
+		sum += row[4].to_i
+	end
+	puts "ruby used " + rb.to_s + " times"
+	puts "python used " + py.to_s + " times"
+	puts "shell used " + sh.to_s + " times"
+	puts "student class numbers sum: " + sum.to_s
+end
+
+def parse_file
+	result = 0
+	File.new("test.csv", 'r').each_line("\n") do |row|
 		columns = row.split(",")
-		puts columns[5]
 		result += columns[5].to_i
 	end
+	puts result
 end
-puts result
+
+parse_csv
