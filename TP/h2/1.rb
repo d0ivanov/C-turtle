43215bc190e4d6757288c0d16bf62b73d9858bd9
starbust <syliconsynapse@gmail.com>
2012-10-04 23:31:16 +0300

    file/folder name change

---
+++ TP/h2/1.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+#execute with either ruby filename.rb
+#or chmod +x filename.rb and ./filename.rb
+require "csv"
+
+def parse_csv
+	py = 0
+	rb = 0
+	sh = 0
+	sum = 0
+	CSV.foreach("test.csv") do |row|
+		case row[5].downcase
+			when "ruby" then rb += 1
+			when "python" then py += 1
+			when "shell" then sh += 1
+		end
+		sum += row[4].to_i
+	end
+	puts "ruby used " + rb.to_s + " times"
+	puts "python used " + py.to_s + " times"
+	puts "shell used " + sh.to_s + " times"
+	puts "student class numbers sum: " + sum.to_s
+end
+
+def parse_file
+	result = 0
+	File.new("test.csv", 'r').each_line("\n") do |row|
+		columns = row.split(",")
+		result += columns[5].to_i
+	end
+	puts result
+end
+
+parse_csv
