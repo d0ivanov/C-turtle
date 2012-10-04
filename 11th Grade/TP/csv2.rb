65b59e82ad81106c24c4c36853bea501f2769c14
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2012-10-04 08:10:38 +0300

    TP homework

---
+++ 11th Grade/TP/csv2.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+pythonRes = 0
+pearlRes = 0
+rubyRes = 0
+
+csv_read
+CSV.foreach("form2.csv") do |row|
+	if row[7] == "Python"
+		pythonRes += row[5].to_i
+	end
+	if row[7] == "Pearl"
+		pearlRes += row[5].to_i
+	end
+	if row[7] == "Ruby"
+		rubyRes += row[5].to_i
+	end
+end
+
+puts "Python: ", pythonRes, " Pearl: ", pearlRes, " Ruby: ", rubyRes
