b36b631936b286eec90f6b5e47bbede447618650
starbust <syliconsynapse@gmail.com>
2012-10-09 08:29:50 +0300

    Added csv file to homework 2. Spilt homework 2 and 3 in separate files.

--- TP/h2/1.rb
+++ TP/h2/1.rb
@@ -4,22 +4,11 @@
 require "csv"
 
 def parse_csv
-	py = 0
-	rb = 0
-	sh = 0
-	sum = 0
-	CSV.foreach("test.csv") do |row|
-		case row[5].downcase
-			when "ruby" then rb += 1
-			when "python" then py += 1
-			when "shell" then sh += 1
-		end
-		sum += row[4].to_i
+	res = 0
+	CSV.foreach("2.csv") do |row|
+		res += row[5].to_i
 	end
-	puts "ruby used " + rb.to_s + " times"
-	puts "python used " + py.to_s + " times"
-	puts "shell used " + sh.to_s + " times"
-	puts "student class numbers sum: " + sum.to_s
+	puts res
 end
 
 def parse_file
