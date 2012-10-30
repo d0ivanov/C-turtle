3d741898e3273c5600a4e3dab091ec5ac802837c
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2012-10-30 08:38:28 +0200

    A yellow belt

---
+++ 11th Grade/TP/Classwork/exam/exam2.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+times = Array.new
+
+count = 0
+CSV.foreach("feature_scenarios_times.csv") do |row|
+	times[count] = row[0].to_f
+	count += 1
+end
+
+sum = 0
+for i in 0...10
+	puts times[i]
+	sum += times[i]
+end
+
+CSV.open("res.csv", "w") do |csv|
+	csv << [sum]
+end
