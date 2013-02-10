6d69ca85bbc770d8badc98aff841994a6033f7a3
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2013-02-11 01:39:37 +0200

    'crawl' function is fixed, so now works even if you call it inside 'def'. Function 'hash-unique-elements' added. Function 'main' added (unfinished)

--- 11th Grade/TP/Web Crawler/web-crawler2/src/web_crawler2/core.clj
+++ 11th Grade/TP/Web Crawler/web-crawler2/src/web_crawler2/core.clj
@@ -14,7 +14,7 @@
 	(let [targets (get-links source)]
 		(write-to-hf (str ":" source " " (seq targets) "\n") true)
 		(if (< current-depth max-depth)
-			(map crawl targets (repeat (inc current-depth)) (repeat max-depth))
+			(doall (map crawl targets (repeat (inc current-depth)) (repeat max-depth)))
 			(if (= current-depth 0)
 				(do (write-to-hf "}" true)
 				 targets)
@@ -22,8 +22,19 @@
 
 (defn read-hash-unique []
 	(into {}
-  		(with-open [r (reader "hash.txt")]
+  	(with-open [r (reader "hash.txt")]
 	 		(doall (distinct
-				(map #(read-string
-					(str "[" (trim (replace % #"[{}]" "")) "]"))
-						(line-seq r)))))))
+							(map #(read-string
+											(str "[" (replace % #"[{}]" "")) "]")
+								(line-seq r)))))))
+
+(defn get-unique-elements [hashmap]
+	(distinct (flatten (for [x hashmap] (x 1)))))
+
+(defn dijkstra-like-algo [hashmap])
+
+(defn main [index max-depth]
+	(crawl index 0 max-depth)
+	(let [hashmap read-hash-unique
+		  	unique-elements (get-unique-elements hashmap)
+		  	unique-elements-num (count unique-elements)]))
