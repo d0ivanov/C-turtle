e7a7608ecef837ac12513c3be5e570468398c248
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2013-02-13 11:56:29 +0200

    Algorithm for distance added.

--- 11th Grade/TP/Web Crawler/web-crawler2/src/web_crawler2/core.clj
+++ 11th Grade/TP/Web Crawler/web-crawler2/src/web_crawler2/core.clj
@@ -12,7 +12,7 @@
 (defn crawl [source current-depth max-depth]
 	(if (= current-depth 0) (write-to-hf "{" false))
 	(let [targets (get-links source)]
-		(write-to-hf (str ":" source " " (seq targets) "\n") true)
+		(write-to-hf (str ":" source " " (vec (map keyword targets)) "\n") true)
 		(if (< current-depth max-depth)
 			(doall (map crawl targets (repeat (inc current-depth)) (repeat max-depth)))
 			(if (= current-depth 0)
@@ -21,19 +21,29 @@
 				targets))))
 
 (defn read-hash-unique []
-	(into {}
-  	(with-open [r (reader "hash.txt")]
-	 		(doall 
-				(distinct
-					(map #(read-string (str "[" (replace % #"[{}]" "")) "]") (line-seq r)))))))
+	(->> (slurp "hash.txt")
+     	 (re-seq #"[^{} \n]+")
+			 (partition 2)
+			 (map vec)
+			 (into {})))
 
 (defn get-unique-elements [hashmap]
 	(distinct (flatten (for [x hashmap] (x 1)))))
 
-(defn dijkstra-like-algo [hashmap])
+(defn in? [seq target]  
+	(some #(= target %) seq))
+
+(defn distance [src target h] 
+	(if (= src target)
+		0
+		(when (h src)
+			(if (in? (h src) target)
+				1
+				(let [arr (filter #(not= % nil) (map #(distance % target h) (h src)))]
+					(when-not (empty? arr) 
+						(inc (apply min arr))))))))
 
 (defn main [index max-depth]
 	(crawl index 0 max-depth)
-	(let [hashmap read-hash-unique
-		  	unique-elements (get-unique-elements hashmap)
-		  	unique-elements-num (count unique-elements)]))
+	(let [unique-elements (set (get-unique-elements (read-hash-unique)))]
+		(distance index :http://www.bard.bg/genres/?id=1 unique-elements)))
